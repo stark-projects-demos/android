fc70c92d135580d75c366ff8948c402eaa0c4062
Daniel Hrivnak <dhrivnak@gmail.com>
2025-07-24 09:18:43 -0500

    Fixed app crashes
    Registered MenusDemoActivity in AndroidManifest.xml for navigation and added a placeholder sortByName(View) method in SemanticsDemoActivity to support app functionality.

--- app/src/main/java/co/getstark/starkandroidsample/demos/SemanticsDemoActivity.java
+++ app/src/main/java/co/getstark/starkandroidsample/demos/SemanticsDemoActivity.java
@@ -1,6 +1,7 @@
 package co.getstark.starkandroidsample.demos;
 
 import android.os.Bundle;
+import android.view.View;
 
 import co.getstark.starkandroidsample.R;
 
@@ -10,5 +11,9 @@ public class SemanticsDemoActivity extends BaseDemoActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_semantics_demo);
         setTitle("Semantic Structure and Meaning - Accessibility Issues");
+    }
+
+    public void sortByName(View view) {
+        // Placeholder to make the app function
     }
 }
